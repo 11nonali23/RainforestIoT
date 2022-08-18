8b187b69913a95d92d72816504b69b8573c8e11d
Andrea Nonali <nonali.andrea@gmail.com>
2022-08-18 17:31:52 +0200

    added tab layout

--- ui.py
+++ ui.py
@@ -1,7 +1,6 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
-from turtle import update
 
 from available_sensors import SENSORS
 
@@ -10,26 +9,38 @@ class Ui(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        windows = tk.Tk()
-        windows.title("My Application")
+        window = tk.Tk()
+        window.title("Rainforest Application")
 
-        self.label = tk.Label(windows, text="Hello World")
-        self.label.grid(column=0, row=0)
+        tabControl = ttk.Notebook(window)
+        tab_environment = ttk.Frame(tabControl)
+        tab_animals = ttk.Frame(tabControl)
 
-        self.label1 = tk.Label(windows)
-        self.label1.grid(column=0, row=1)
+        tabControl.add(tab_environment, text='Environment')
+        tabControl.add(tab_animals, text='Animals')
+        tabControl.pack(expand=1, fill="both")
 
-        self.label2 = tk.Label(windows)
-        self.label2.grid(column=0, row=2)
+        # Set other parts of UI in another class/method
+
+        ttk.Label(
+            tab_environment,
+            text="Qui ci metti le hose"
+        ).grid(column=0, row=0, padx=30, pady=30)
+
+        ttk.Label(
+            tab_animals,
+            text="Qui ci metti le altre hose"
+        ).grid(column=0, row=0, padx=30, pady=30)
 
     def update(self):
         self._update_data()
         self.after(2000, self.update)
 
     def _update_data(self):
-        self.label1.configure(
-            text=f'User Presence Sensor value {SENSORS[0].value}'
-        )
-        self.label2.configure(
-            text=f'Water Vaporizer value {SENSORS[1].value}'
-        )
+        # self.label1.configure(
+        #     text=f'User Presence Sensor value {SENSORS[0].value}'
+        # )
+        # self.label2.configure(
+        #     text=f'Water Vaporizer value {SENSORS[1].value}'
+        # )
+        pass
