ed7f10ed104feffbe3e455514ba01614e8bbd618
Andrea Nonali <nonali.andrea@gmail.com>
2022-08-20 13:36:27 +0200

    add sensors and re-index

--- sensors/available_sensors.py
+++ sensors/available_sensors.py
@@ -2,26 +2,27 @@ from sensors.sensor import AnalogicalSensor, DigitalSensor, JSONSensor
 
 SENSORS = [
     AnalogicalSensor(_id="1", name="User Presence", value=True),
-    AnalogicalSensor(_id="2", name="Water Spout", value=False),
-    AnalogicalSensor(_id="3", name="Irrigators", value=False),
+    AnalogicalSensor(_id="2", name="Water Vaporizer", value=False),
+    DigitalSensor(_id="3", name="Vaporizer Power", value=30),
+    AnalogicalSensor(_id="4", name="Irrigators", value=False),
+    DigitalSensor(_id="5", name="Irrigator Power", value=30),
 
-    DigitalSensor(_id="4", name="Humidity", value=30),
-    DigitalSensor(_id="5", name="Temperature", value=30),
-    DigitalSensor(_id="6", name="Soil Moisture", value=5),
+    DigitalSensor(_id="6", name="Humidity", value=30),
+    DigitalSensor(_id="7", name="Soil Moisture", value=5),
 
-    JSONSensor(_id="7", name="Bird1 Health", value={"hb": 35, "body_tem": 35}),
-    JSONSensor(_id="8", name="Bird2 Health", value={"hb": 35, "body_tem": 35}),
-    JSONSensor(_id="9", name="Bird3 Health", value={"hb": 35, "body_tem": 35}),
+    JSONSensor(_id="8", name="Bird1 Health", value={"hb": 35, "body_tem": 35}),
+    JSONSensor(_id="9", name="Bird2 Health", value={"hb": 35, "body_tem": 35}),
+    JSONSensor(_id="10", name="Bird3 Healt", value={"hb": 35, "body_tem": 35}),
 
-    JSONSensor(_id="10", name="Bird1 Coords", value={"lat": 45, "lon": 46}),
-    JSONSensor(_id="11", name="Bird2 Coords", value={"lat": 45, "lon": 46}),
-    JSONSensor(_id="12", name="Bird3 Coords", value={"lat": 45, "lon": 46}),
+    JSONSensor(_id="11", name="Bird1 Coords", value={"lat": 45, "lon": 46}),
+    JSONSensor(_id="12", name="Bird2 Coords", value={"lat": 45, "lon": 46}),
+    JSONSensor(_id="13", name="Bird3 Coords", value={"lat": 45, "lon": 46}),
 
-    AnalogicalSensor(_id="13", name="HearthBeat Alarm", value=False),
-    AnalogicalSensor(_id="14", name="Body Temperature Alarm", value=False),
-    AnalogicalSensor(_id="15", name="Boundaries Alarm", value=False),
-    AnalogicalSensor(_id="16", name="Humidity Alarm", value=False),
-    AnalogicalSensor(_id="17", name="Soil Moisture Alarm", value=False),
+    AnalogicalSensor(_id="14", name="HearthBeat Alarm", value=False),
+    AnalogicalSensor(_id="15", name="Body Temperature Alarm", value=False),
+    AnalogicalSensor(_id="16", name="Boundaries Alarm", value=False),
+    AnalogicalSensor(_id="17", name="Humidity Alarm", value=False),
+    AnalogicalSensor(_id="18", name="Soil Moisture Alarm", value=False),
 ]
 
 
